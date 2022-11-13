e34f504c62bca4713a9395c0aa7452eb85f76dee
Vladislav Klass <vladislav.klass@tum.de>
2022-11-13 21:23:46 +0100

    remove pricing from home page

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -90,7 +90,7 @@ export default function Pricing({ products }: Props) {
             Control your terminal with natural language
           </h1>
 
-          {/* Embed a gif with padding above and below*/}
+          {/* Embed a iframe here*/}
           <div className="justify-center" ref={containerRef}>
           </div>
 
@@ -98,13 +98,10 @@ export default function Pricing({ products }: Props) {
             Use your command line intuitively by typing in natural language.
             Kiara works with OpenAI's powerful Codex AI which also powers GitHub Copilot. <br></br><br></br>
 
-            Try it out for free!
+            Try it out now for free!
           </p>
 
-
-          {/* provide installation instructions for pip */}
           <div className="justify-center">
-            {/* add more padding vertically */}
             <div className="mt-8 mb-8">
               <p className="mt-5 text-xl text-zinc-200 sm:text-center sm:text-2xl max-w-2xl m-auto">
                 Install Kiera with pip:
@@ -127,144 +124,12 @@ export default function Pricing({ products }: Props) {
               </pre>
 
 
-              {/* add more padding */}
               <p className="mt-16 text-xl text-zinc-200 sm:text-center sm:text-2xl max-w-2xl m-auto">
                 Any questions? <a className="text-pink-500 underline" href="info@kiera.ai">Contact us</a>
               </p>
             </div>
           </div>
-
-
-          <div className="relative self-center mt-6 bg-zinc-900 rounded-lg p-0.5 flex sm:mt-8 border border-zinc-800">
-            <button
-              onClick={() => setBillingInterval('month')}
-              type="button"
-              className={`${billingInterval === 'month'
-                ? 'relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white'
-                : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
-                } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
-            >
-              Monthly billing
-            </button>
-            <button
-              onClick={() => setBillingInterval('year')}
-              type="button"
-              className={`${billingInterval === 'year'
-                ? 'relative w-1/2 bg-zinc-700 border-zinc-800 shadow-sm text-white'
-                : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
-                } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
-            >
-              Yearly billing
-            </button>
-          </div>
         </div>
-        <div className="mt-12 space-y-4 sm:mt-16 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6 lg:max-w-4xl lg:mx-auto xl:max-w-none xl:mx-0 xl:grid-cols-2">
-          {products
-            .map((product) => {
-              const price = product?.prices?.find(
-                (price) => price.interval === billingInterval
-              );
-              if (!price) return null;
-              const priceString = new Intl.NumberFormat('en-US', {
-                style: 'currency',
-                currency: price.currency,
-                minimumFractionDigits: 0
-              }).format((price?.unit_amount || 0) / 100);
-              return (
-                <div
-                  key={product.id}
-                  className={cn(
-                    'rounded-lg shadow-sm divide-y divide-zinc-600 bg-zinc-900',
-                    {
-                      'border border-pink-500': subscription
-                        ? product.name === subscription?.prices?.products?.name
-                        : product.name === 'Freelancer'
-                    }
-                  )}
-                >
-                  <div className="p-6">
-                    <h2 className="text-2xl leading-6 font-semibold text-white">
-                      {product.name}
-                    </h2>
-                    <p className="mt-4 text-zinc-300">{product.description}</p>
-                    <p className="mt-8">
-                      <span className="text-5xl font-extrabold white">
-                        {priceString}
-                      </span>
-                      <span className="text-base font-medium text-zinc-100">
-                        /{billingInterval}
-                      </span>
-                    </p>
-                    <Button
-                      variant="slim"
-                      type="button"
-                      disabled={isLoading}
-                      loading={priceIdLoading === price.id}
-                      onClick={() => handleCheckout(price)}
-                      className="mt-8 block w-full rounded-md py-2 text-sm font-semibold text-white text-center hover:bg-zinc-900"
-                    >
-                      {product.name === subscription?.prices?.products?.name
-                        ? 'Manage'
-                        : 'Subscribe'}
-                    </Button>
-                  </div>
-                </div>
-              );
-            })}
-        </div>
-        {/* <div>
-          <p className="mt-24 text-xs uppercase text-zinc-400 text-center font-bold tracking-[0.3em]">
-            Brought to you by
-          </p>
-          <div className="flex flex-col items-center my-12 space-y-4 sm:mt-8 sm:space-y-0 md:mx-auto md:max-w-2xl sm:grid sm:gap-6 sm:grid-cols-5">
-            <div className="flex items-center justify-start">
-              <a href="https://nextjs.org" aria-label="Next.js Link">
-                <img
-                  src="/nextjs.svg"
-                  alt="Next.js Logo"
-                  className="h-12 text-white"
-                />
-              </a>
-            </div>
-
-            <div className="flex items-center justify-start">
-              <a href="https://vercel.com" aria-label="Vercel.com Link">
-                <img
-                  src="/vercel.svg"
-                  alt="Vercel.com Logo"
-                  className="h-6 text-white"
-                />
-              </a>
-            </div>
-            <div className="flex items-center justify-start">
-              <a href="https://stripe.com" aria-label="stripe.com Link">
-                <img
-                  src="/stripe.svg"
-                  alt="stripe.com Logo"
-                  className="h-12 text-white"
-                />
-              </a>
-            </div>
-            <div className="flex items-center justify-start">
-              <a href="https://supabase.io" aria-label="supabase.io Link">
-                <img
-                  src="/supabase.svg"
-                  alt="supabase.io Logo"
-                  className="h-10 text-white"
-                />
-              </a>
-            </div>
-            <div className="flex items-center justify-start">
-              <a href="https://github.com" aria-label="github.com Link">
-                <img
-                  src="/github.svg"
-                  alt="github.com Logo"
-                  className="h-8 text-white"
-                />
-              </a>
-            </div>
-          </div>
-        </div> */}
       </div>
     </section>
   );
